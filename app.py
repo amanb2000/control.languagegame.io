898f41499e1ef991e1a0990e2e8c957ac072bb10
Aman Bhargava <amanb2000@gmail.com>
2024-04-28 01:20:47 -0700

    Fully working at conway.languagegame.io/inference

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 def inference_call(req: GenRequest, API_URL, num_tokens=50):
-    print("[app.py] making inference call with api url = ", API_URL)
     response = requests.post(API_URL, json=req.model_dump())
     if response.status_code == 200:
         return response.json()
@@ -16,11 +15,11 @@ def inference_call(req: GenRequest, API_URL, num_tokens=50):
         print(f"Error {response.status_code}: {response.text}")
         return None
 
-@app.get("/")
+@app.get("/inference")
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post("/generate")
+@app.post("/inference/generate")
 async def generate(request: Request):
     form_data = await request.form()
     system_prompt = form_data.get("system_prompt", "")
@@ -38,12 +37,3 @@ async def generate(request: Request):
         return {"generated": result["generated"]}
     else:
         return {"generated": "Error occurred during inference."}
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--host", type=str, default="0.0.0.0", help="Host address")
-    parser.add_argument("--port", type=int, default=8000, help="Port number")
-    args = parser.parse_args()
-
-    import uvicorn
-    uvicorn.run(app, host=args.host, port=args.port)
