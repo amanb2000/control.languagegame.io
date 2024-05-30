366b109618b54a4c7cf5dc2d7a54f14027edfe12
Aman Bhargava <amanb2000@gmail.com>
2024-05-30 00:29:58 -0700

    Fix recompute leaderboard script, does not error out anymore

--- recompute_leaderboard.py
+++ recompute_leaderboard.py
@@ -33,13 +33,17 @@ wins.
 import os
 import re
 from datetime import datetime
+import pandas as pd
 import pdb
 
 MAX_LEN=32
+MAX_LEADERBOARD = 1000
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 telemetry_folder = os.path.join(script_dir, 'telemetry')
 leaderboard_file = os.path.join(script_dir, 'leaderboard.txt')
+full_leaderboard_file = os.path.join(script_dir, 'full_leaderboard.txt')
+nodups_leaderboard_file = os.path.join(script_dir, 'nodups_leaderboard.txt')
 logfile = os.path.join(script_dir, 'recompute_leaderboard.log')
 
 # add an entry to the logfile 
@@ -102,7 +106,11 @@ def process_telemetry_folder(folder_path):
     for filename in os.listdir(folder_path):
         if filename.endswith('.txt'):
             file_path = os.path.join(folder_path, filename)
-            telemetry_data = read_telemetry_file(file_path)
+            try: 
+                telemetry_data = read_telemetry_file(file_path)
+            except Exception as e: 
+                print("Unable to read telemetry data for file: ", file_path)
+                print("Exception: ", e)
 
             nickname = telemetry_data['Nickname']
             control_input = telemetry_data['System Prompt']
@@ -146,11 +154,31 @@ def process_telemetry_folder(folder_path):
     # remove all entries with reached desired output = False
     #if any(entry['reached desired output'] for entry in leaderboard_entries):
         # leaderboard_entries = [entry for entry in leaderboard_entries if entry['reached desired output']]
-
+    cnt=0
     with open(leaderboard_file, 'w', newline='') as f:
         f.write('nickname,control input,ce loss,k,generated text,desired output,time of request,reached desired output\n')
         for entry in leaderboard_entries:
             f.write(f"{entry['nickname']},{entry['control input']},{entry['ce loss']},{entry['k']},{entry['generated text']},{entry['desired output']},{entry['time of request']},{entry['reached desired output']}\n")
+            cnt += 1
+            if cnt > MAX_LEADERBOARD: 
+                break
+    
+    # now write to the full leaderboard
+    with open(full_leaderboard_file, 'w', newline='') as f:
+        f.write('nickname,control input,ce loss,k,generated text,desired output,time of request,reached desired output\n')
+        for entry in leaderboard_entries:
+            f.write(f"{entry['nickname']},{entry['control input']},{entry['ce loss']},{entry['k']},{entry['generated text']},{entry['desired output']},{entry['time of request']},{entry['reached desired output']}\n")
+
+
 
 if __name__ == '__main__':
     process_telemetry_folder(telemetry_folder)
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"[{current_time}] Recomputed leaderboard. Check leaderboard.txt for the updated leaderboard.")
+    print(f"Now computing nodups_full_leaderboard.txt...")
+    # run the script located at compute_nodups_leaderboard.py -- located in the same
+    # directory as this script
+    nodup_script_path = os.path.join(script_dir, 'compute_nodups_leaderboard.py')
+    os.system(f'/home/aman/control.languagegame.io/venv/bin/python3 {nodup_script_path}')
+    print(f"[{current_time}] Done computing nodups_full_leaderboard.txt.")
+    log(f"{current_time}: Done computing nodups_full_leaderboard.txt, done computing leaderboard.txt")
